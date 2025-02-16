0b468b34efc64e1e5cc75f698e37c96c20b4191b
Eoghan Crowley <eoghan@riskycrayon.com>
2025-02-16 13:18:17 +0000

    feat(types): expand Animal type to include stats

--- src/types/animal.types.ts
+++ src/types/animal.types.ts
@@ -1,4 +1,11 @@
+interface AnimalStats {
+    happiness: number;
+    hunger: number;
+    sleep: number;
+}
+
 export interface Animal {
     id: string;
     name: string;
+    stats: AnimalStats;
 }
